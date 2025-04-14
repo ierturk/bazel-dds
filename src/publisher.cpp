94055f473fb8285a8f4763f0ae30a809ccf965c1
Ibrahim Erturk <me@erturk.me>
2025-04-14 18:53:30 +0300

    Initial commit

---
+++ src/publisher.cpp
@@ -0,0 +1,41 @@
+#include "HelloWorldPubSubTypes.h"
+
+#include <fastdds/dds/domain/DomainParticipantFactory.hpp>
+#include <fastdds/dds/domain/DomainParticipant.hpp>
+#include <fastdds/dds/publisher/Publisher.hpp>
+#include <fastdds/dds/topic/Topic.hpp>
+#include <fastdds/dds/publisher/DataWriter.hpp>
+#include <iostream>
+
+using namespace eprosima::fastdds::dds;
+using namespace HelloWorldModule;
+
+int main() {
+    // Create DomainParticipant
+    DomainParticipantQos participant_qos;
+    DomainParticipant* participant = DomainParticipantFactory::get_instance()->create_participant(0, participant_qos);
+
+    // Register the type
+    TypeSupport type(new HelloWorldPubSubType());
+    participant->register_type(type);
+
+    // Create Topic
+    Topic* topic = participant->create_topic("HelloWorldTopic", "HelloWorld", TOPIC_QOS_DEFAULT);
+
+    // Create Publisher
+    Publisher* publisher = participant->create_publisher(PUBLISHER_QOS_DEFAULT);
+
+    // Create DataWriter
+    DataWriter* writer = publisher->create_datawriter(topic, DATAWRITER_QOS_DEFAULT);
+
+    // Publish data
+    HelloWorld data;
+    data.message("Hello from Fast DDS!");
+    while (true) {
+        writer->write(&data);
+        std::cout << "Published: " << data.message() << std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+
+    return 0;
+}
---
+++ src/subscriber.cpp
@@ -0,0 +1,54 @@
+#include "HelloWorldPubSubTypes.h"
+
+#include <fastdds/dds/domain/DomainParticipantFactory.hpp>
+#include <fastdds/dds/domain/DomainParticipant.hpp>
+#include <fastdds/dds/subscriber/Subscriber.hpp>
+#include <fastdds/dds/topic/Topic.hpp>
+#include <fastdds/dds/subscriber/DataReader.hpp>
+#include <fastdds/dds/subscriber/DataReaderListener.hpp>
+#include <fastdds/dds/subscriber/SampleInfo.hpp>
+#include <iostream>
+
+
+using namespace eprosima::fastdds::dds;
+using namespace HelloWorldModule;
+
+
+class HelloWorldListener : public DataReaderListener {
+public:
+    void on_data_available(DataReader* reader) override {
+        HelloWorldModule::HelloWorld data;
+        SampleInfo info;
+        if (reader->take_next_sample(&data, &info) == ReturnCode_t::RETCODE_OK) {
+            if (info.valid_data) {
+                std::cout << "Received: " << data.message() << std::endl;
+            }
+        }
+    }
+};
+
+int main() {
+    // Create DomainParticipant
+    DomainParticipantQos participant_qos;
+    DomainParticipant* participant = DomainParticipantFactory::get_instance()->create_participant(0, participant_qos);
+
+    // Register the type
+    TypeSupport type(new HelloWorldPubSubType());
+    participant->register_type(type);
+
+    // Create Topic
+    Topic* topic = participant->create_topic("HelloWorldTopic", "HelloWorld", TOPIC_QOS_DEFAULT);
+
+    // Create Subscriber
+    Subscriber* subscriber = participant->create_subscriber(SUBSCRIBER_QOS_DEFAULT);
+
+    // Create DataReader
+    HelloWorldListener listener;
+    DataReader* reader = subscriber->create_datareader(topic, DATAREADER_QOS_DEFAULT, &listener);
+
+    // Wait for data
+    std::cout << "Waiting for data..." << std::endl;
+    std::this_thread::sleep_for(std::chrono::seconds(10));
+
+    return 0;
+}
